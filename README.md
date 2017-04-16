57055fb6c3660c5932c9d6df710bd7d11ade7f89
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-04-16 18:09:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Teeworlds-ratings-web
+=====================
+
+Webapp to display the information from https://github.com/nzyuzin/teeworlds-ratings database.
+
+Dependencies
+------------
+
+ruby, sqlite3
+
+Gems: sinatra, haml, sinatra-paginate
+
+Using
+-----
---
+++ rctf.rb
@@ -0,0 +1,34 @@
+require 'sinatra/base'
+require 'sinatra/paginate'
+require 'sqlite3'
+
+Struct.new('Result', :total, :size, :players)
+
+class TeeworldsRatings < Sinatra::Base
+  register Sinatra::Paginate
+
+  helpers do
+    def page
+      [params[:page].to_i - 1, 0].max
+    end
+  end
+
+  set :haml, :format => :html5
+
+  def get_players()
+    db = SQLite3::Database.new("rctf.db")
+    rows = db.execute("select name, clan, rating from players")
+    db.close()
+    rows
+  end
+
+  get '/' do
+    players_per_page = 50
+    player_rows = get_players()
+    displayed_players = player_rows[page * players_per_page, players_per_page]
+    @players = Struct::Result.new(player_rows.length, displayed_players.length, displayed_players)
+    haml :index, :locals => {:players_per_page => players_per_page}
+  end
+
+  run! if app_file == $0
+end
