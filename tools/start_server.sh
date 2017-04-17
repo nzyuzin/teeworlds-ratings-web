db40b768cbcd16b6f33b772e27da8a19cbb9509c
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-04-17 16:38:44 +0200

    Bind server to 0.0.0.0

--- tools/start_server.sh
+++ tools/start_server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -e ./tools/server_pid ]; then
-  bundle exec rackup -P ./tools/server_pid &> /dev/null &
+  bundle exec rackup -P ./tools/server_pid -o 0.0.0.0 &> /dev/null &
 else
   echo "Server is running already."
 fi
