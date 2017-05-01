4f5fd6126e0ec8fee1ec556d2ffff93e2d57970e
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-05-01 11:12:47 +0200

    Register player in model callback instead of registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,24 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, :only => [:create, :update]
 
-  def register_player(name, clan)
-    request = {:message_type => "external_message", :message_content =>
-     {:external_message_type => "registration_request", :external_message_content =>
-      {:registration_request_type => "register", :registration_request_content =>
-       {:name => name, :clan => clan}}}}
-    request_json = JSON.generate(request)
-    s = TCPSocket.new '127.0.0.1', 12488
-    s.puts request_json
-    response_json = s.gets
-    s.close
-    answer = JSON.parse(response_json, symbolize_names: true)
-  end
-
-  def create
-    super
-    register_player(params[:user][:player_name], params[:user][:player_clan]) unless @user.invalid?
-  end
-
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:player_name, :player_clan, :country, :email, :password) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :timeoutable, :lockable
 
+  validates :player_name, :presence => true
   validate :player_name_should_be_available
+
+  before_create :register_player
 
   def player_name_should_be_available
     request = {:message_type => "external_message", :message_content =>
@@ -19,6 +22,19 @@ class User < ApplicationRecord
     s.close
     answer = JSON.parse(response_json, symbolize_names: true)
     errors.add(:player_name, "This player name is already in use") unless answer[:message_content][:external_message_content][:registration_request_response_content] == true
+  end
+
+  def register_player
+    request = {:message_type => "external_message", :message_content =>
+     {:external_message_type => "registration_request", :external_message_content =>
+      {:registration_request_type => "register", :registration_request_content =>
+       {:name => self.player_name, :clanb => self.player_clan}}}}
+    request_json = JSON.generate(request)
+    s = TCPSocket.new '127.0.0.1', 12488
+    s.puts request_json
+    response_json = s.gets
+    s.close
+    answer = JSON.parse(response_json, symbolize_names: true)
   end
 
 end
