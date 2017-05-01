433248a3236ee925913ff7a21b1db38a8a8690d8
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-05-01 20:21:02 +0200

    Remove debug printing

--- app/models/teeworlds_entity.rb
+++ app/models/teeworlds_entity.rb
@@ -20,7 +20,6 @@ class TeeworldsEntity
     s.puts request_json
     response_json = s.gets
     s.close
-    puts response_json
     JSON.parse(response_json, symbolize_names: true)[:message_content][:external_message_content][:data_request_response_content]
   end
 
