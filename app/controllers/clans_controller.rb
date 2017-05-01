2b7d116757659edb0ecf934fbe2a2e60d885831b
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-05-02 01:30:56 +0200

    Move clan queries from controller to model

--- app/controllers/clans_controller.rb
+++ app/controllers/clans_controller.rb
@@ -1,25 +1,9 @@
 class ClansController < ApplicationController
 
-  def get_clan(name)
-    request = {:message_type => "external_message", :message_content =>
-     {:external_message_type => "data_request", :external_message_content =>
-      {:data_request_type => "clan_info", :data_request_content =>
-       {:name => name}}}}
-    request_json = JSON.generate(request)
-    s = TCPSocket.new '127.0.0.1', 12488
-    s.puts request_json
-    response_json = s.gets
-    s.close
-    clan_json = JSON.parse(response_json, symbolize_names: true)
-    clan = clan_json[:message_content][:external_message_content][:data_request_response_content]
-    res = Clan.parse(clan[:clan])
-    res.players = clan[:players].map { |p| Player.parse p }
-    res
-  end
-
   def show
     clan_name = params[:clan_name]
-    @clan = get_clan clan_name
+    @clan = Clan.clan_info(clan_name)
     @players = @clan.players
   end
+
 end
--- app/models/clan.rb
+++ app/models/clan.rb
@@ -1,4 +1,4 @@
-class Clan
+class Clan < TeeworldsEntity
   attr_accessor :name, :rating, :players
 
   def self.parse(clan_hash)
@@ -7,4 +7,16 @@ class Clan
     res.rating = clan_hash[:clan_rating]
     res
   end
+
+  def self.request_clan_info(name)
+    request_data(DATA_REQUEST_TYPE_CLAN_INFO, {:name => name})
+  end
+
+  def self.clan_info(name)
+    clan = request_clan_info(name)
+    res = parse(clan[:clan])
+    res.players = clan[:players].map { |p| Player.parse p }
+    res
+  end
+
 end
